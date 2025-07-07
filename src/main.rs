68f9118026a09a46f51534373b703cf0e4c76d1e
Raoul Kaleba <raouldev@outlook.com>
2025-07-07 11:25:03 -0500

    fixed localhost only server

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod responses;
 #[tokio::main]
 async fn main() {
     // Create new server on localhost:3000
-    let server = server_manager::Server::new("127.0.0.1:3000");
+    let server = server_manager::Server::new("0.0.0.0:3000");
     
     // Start the server
     server.start().await;
