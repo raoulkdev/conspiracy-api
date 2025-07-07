9bdcfe7bd6285156deacc4f7a522662bd0091966
Raoul Kaleba <raouldev@outlook.com>
2025-07-07 11:23:43 -0500

    more cleanup

--- src/server_manager.rs
+++ src/server_manager.rs
@@ -7,40 +7,40 @@ use crate::responses::{all_categories, all_theories, theories_by_category, theor
 
 // Server struct
 pub struct Server {
-    address: & 'static str
+    address: &'static str,
 }
 
 // Server implementation
 impl Server {
     // Create a new server instance
-    pub fn new(address: & 'static str) -> Self {
-        Self {address}
+    pub fn new(address: &'static str) -> Self {
+        Self { address }
     }
-    
+
     // Start server
     pub async fn start(&self) {
         // Bind TCP listener to address
-        let listener = TcpListener::bind(&self.address).await;
-        
-        // Check if binding was successfull
-        match listener { 
-            Ok(a) => {
-                println!("{}{}", "Server successfully started on: ".bright_green(), a.local_addr().unwrap());
-                axum::serve(a, router()).await.unwrap();
+        match TcpListener::bind(self.address).await {
+            Ok(listener) => {
+                println!(
+                    "{}{}",
+                    "Server successfully started on: ".bright_green(),
+                    listener.local_addr().unwrap()
+                );
+                axum::serve(listener, router()).await.unwrap();
             }
             Err(e) => {
-                println!("{}{}", "Could not start server: ", e)
+                println!("{}{}", "Could not start server: ".bright_red(), e);
             }
-        }
-        
-        // Endpoint router
-        fn router() -> Router {
-            Router::new()
-                .route("/theories", get(all_theories))
-                .route("/theories/{capture}", get(theory_by_id))
-                .route("/theories/", get(theories_by_category))
-                .route("/categories", get(all_categories))
         }
     }
 }
 
+// Endpoint router
+fn router() -> Router {
+    Router::new()
+        .route("/theories", get(all_theories))
+        .route("/theories/{capture}", get(theory_by_id))
+        .route("/theories/", get(theories_by_category))
+        .route("/categories", get(all_categories))
+}
